44817160ba65f83947c9e9ee91285375b278d989
Luke Rotherfield <luke@oxygenthinking.com>
2012-11-07 13:06:03 +0000

    travis updates

---
+++ Tests/Controller/PageControllerTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Savvy\PagesBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class PageControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        //to be done
+        $client = static::createClient();
+
+        $this->assertTrue(true);
+    }
+}
