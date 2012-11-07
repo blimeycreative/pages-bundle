661bca9609685e78c4e01ddd47f67e5ecedee210
Luke Rotherfield <luke@oxygenthinking.com>
2012-11-07 13:25:24 +0000

    empty test

---
+++ Tests/DefaultControllerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Savvy\PagesBundle\Tests\Controller;
+
+
+class PageControllerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testIndex()
+    {
+        //to be done
+        
+    }
+}
