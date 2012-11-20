654b528ba72673436523b3ce8a8d40a0f1eb168f
Luke Rotherfield <luke@oxygenthinking.com>
2012-11-20 14:02:32 +0000

    image compression

--- Controller/PageController.php
+++ Controller/PageController.php
@@ -69,7 +69,7 @@ class PageController extends BaseController
                             } else {
                                 $im->thumbnailImage($width, $height);
                             }
-                            $im->setCompression(imagick::COMPRESSION_ZIP);
+                            $im->setCompression(\Imagick::COMPRESSION_ZIP);
                             $im->setCompressionQuality(80);
                             $im->writeImage($this->media_cache_route . "$id-$width-$height.{$media->getMediaType()->getExtension()}");
                         }
