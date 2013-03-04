908d40ff33e3a64717930209c99c3aeeec0e45c3
Luke Rotherfield <luke@oxygenthinking.com>
2013-03-04 13:43:59 +0000

    development method for galleries

--- Entity/DevelopmentRepository.php
+++ Entity/DevelopmentRepository.php
@@ -17,7 +17,7 @@ class DevelopmentRepository extends EntityRepository
     public function getConstructionGalleries($development_id, $offset)
     {
         return $this->createQueryBuilder('d')
-            ->select("d, md")
+            ->select("d, g, f, md")
             ->innerJoin("d.cms_galleries", "g")
             ->innerJoin("g.files", "f")
             ->innerJoin("f.media_data", "md")
